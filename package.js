ef401379fc3005ffd7097590d1a7dc732721df47
Anton Tursenev <anton@rikishi.info>
2019-08-30 15:46:31 +0700

    Update us flag

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rikishi:flag-icon',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'CSS for vector based country flags.',
   git: 'https://github.com/Katochimoto/meteor-flag-icon.git',
   documentation: 'README.md'
